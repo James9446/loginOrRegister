efa46d1954a1192a61ce695904882fe9053bb41c
James Reynolds <jameshreynolds@gmail.com>
2017-11-13 11:24:23 -0800

    Login functionality added. Also added a new method on the userObject to reset login input data.

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ var userObject = {
 	addProfile: function(obj) {
 		this.profiles.unshift(obj);
 	},
-	resetUserData: function () {
+	resetUserData: function() {
 		document.getElementById('user-first-name').value = '';
 		document.getElementById('user-last-name').value = '';
 		// store elements by class for radios and select - will loop through later to find user selection
@@ -23,8 +23,13 @@ var userObject = {
 		document.getElementById('choose-password').value = '';
 		document.getElementById('confirm-password').value = '';
 	},
+	resetLoginData: function() {
+		document.getElementById('username').value = '';
+		document.getElementById('password').value = '';
+	},
 	getUserData: function() {
 		var	newProfile;
+		var userGender;
 		// store vaules for user first & last name, email and phone num
 		var userFirst     = document.getElementById('user-first-name').value;
 		var userLast      = document.getElementById('user-last-name').value;
@@ -43,26 +48,18 @@ var userObject = {
 			if(userGenderEls[i].checked) {
 				// set value of answer 3 to the value in the radio item
 				userGender = userGenderEls[i].value;
-			};
-		};
+			}
+		}
 		newProfile = new userObject.constructUserProfile(userFirst, userLast, userGender, userEmail, userPhone, username, userPassword);
 		return newProfile;
 	}
-}
+};
 
 function validatePassword (password) {
 	if (password.length < 6) {
 		return false;
 	} else {
 		return true;
-	}
-}
-
-function confirmPassword(first, second) {
-	if (first === second) {
-		return true;
-	} else {
-		return false;
 	}
 }
 
@@ -76,7 +73,7 @@ function register() {
 		}
 	}
 	if (!validatePassword(newProfile.password)) {
-		return alert("This not a valid password. \nYour password must be at least 6 characters long");
+		return alert("This not a valid password. \nYour password must be at least 6 characters long.");
 	}
 	if (newProfile.password !== confirmPassword) {
 		document.getElementById('confirm-password').value = '';
@@ -84,14 +81,14 @@ function register() {
 	}
 	userObject.addProfile(newProfile);
 	userObject.resetUserData();
+	userObject.resetLoginData();
 	showProfile();
 }
 
-function showProfile() {
-	
-	// make sure the user data is being passed through
-	// console.log(userObject.profiles);
-
+function showProfile(index) {
+	if (index === undefined) {
+		index = 0;
+	}
 	// select some HTML elements by id
 	var userName   = document.getElementById('newUserName');
 	var userGender = document.getElementById('newUserGender');
@@ -100,16 +97,29 @@ function showProfile() {
 	var username   = document.getElementById('newUsername');
 
 	// change the text of these variables using the userProfile object
-	userName.innerText   = "Welcome " + userObject.profiles[0].firstName + " " + userObject.profiles[0].lastName;
-	userGender.innerText = "Gender: " + userObject.profiles[0].gender;
-	userEmail.innerText  = "Email: " + userObject.profiles[0].email;
-	userPhone.innerText  = "Phone: " + userObject.profiles[0].number;
-	username.innerText   = "Username: " + userObject.profiles[0].username;
+	userName.innerText   = "Welcome " + userObject.profiles[index].firstName + " " + userObject.profiles[index].lastName;
+	userGender.innerText = "Gender: " + userObject.profiles[index].gender;
+	userEmail.innerText  = "Email: " + userObject.profiles[index].email;
+	userPhone.innerText  = "Phone: " + userObject.profiles[index].number;
+	username.innerText   = "Username: " + userObject.profiles[index].username;
 
-	// hide new profile form
+	// hide new login form or profile form
+	document.getElementById('login-form').style.display = "none";
 	document.getElementById('register-form').style.display = "none";
 	// display updated profile data
 	document.getElementById('show-profile').style.display = "block";
+}
+
+function login() {
+	var username = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
+	for (var i = 0; i < userObject.profiles.length; i++) {
+		if (username === userObject.profiles[i].username && password === userObject.profiles[i].password) {			
+			userObject.resetLoginData();
+			return showProfile(i);
+		} 
+	}
+	return alert("Your username and password do not match or are not correct");
 }
 
 
@@ -143,6 +153,7 @@ function signOut() {
 
 // Event listeners for buttons
 document.getElementById('registerButton').addEventListener('click', register, false);
+document.getElementById('loginButton').addEventListener('click', login, false);
 document.getElementById('showRegisterButton').addEventListener('click', showRegistrationForm, false);
 document.getElementById('showLoginButton').addEventListener('click', showLoginForm, false);
 document.getElementById('signOutButton').addEventListener('click', signOut, false);
